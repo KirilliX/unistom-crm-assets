c6656b82b1c500dc4055745ed4afa199144aa826
Кир <Kir@MacBook-Air-Inna.local>
2026-08-07 23:14:52 +0500

    Add Unistom Russian UI overrides

---
+++ frontend/translations-custom.js
@@ -0,0 +1,12 @@
+Object.assign(window.translated_messages || (window.translated_messages = {}), {
+  "created this deal": "создал(а) эту сделку",
+  "No Contacts Added": "Контакты не добавлены",
+  "Add Organization...": "Добавить организацию...",
+  "Add Website...": "Добавить сайт...",
+  "Add Territory...": "Добавить территорию...",
+  "Add Annual Revenue...": "Добавить годовой доход...",
+  "Add Closed Date...": "Добавить дату закрытия...",
+  "Add Probability...": "Добавить вероятность...",
+  "Add Next Step...": "Добавить следующий шаг...",
+  "of": "из"
+})
