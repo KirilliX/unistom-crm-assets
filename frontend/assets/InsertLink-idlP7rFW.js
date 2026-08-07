ebd0c04f0e60d422740b4764b7905eb22b549dd1
Кир <Kir@MacBook-Air-Inna.local>
2026-08-07 23:05:48 +0500

    Polish Russian onboarding and call log navigation

--- frontend/assets/InsertLink-idlP7rFW.js
+++
@@ -1,2 +0,0 @@
-import{k as t,y as s,z as a,A as i}from"./index-cOp8Z1rf.js";const c=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{c as default};
-//# sourceMappingURL=InsertLink-idlP7rFW.js.map
